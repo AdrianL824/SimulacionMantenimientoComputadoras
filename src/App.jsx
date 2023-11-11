0a8637e037fd5964053eee2d549a38c182039092
KennyV91 <velasco_ariel_123@hotmail.com>
2023-11-10 21:32:52 -0400

    Grafico de tiempo entre descomposturas

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header.jsx";
 // import Home from "./pages/Home.jsx";
 import Descripcion from "./pages/Description.jsx";
+import Grafico from "./pages/Graficos.jsx"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Descripcion />} />
+        <Route path="/grafico" element={<Grafico />} />
       </Routes>
     </Router>
   );
