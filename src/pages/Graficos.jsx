8524b834980b5bcf01dde6196132dc52760fd226
KennyV91 <velasco_ariel_123@hotmail.com>
2023-11-11 15:51:43 -0400

    Grafico Estandarizado modificado

--- src/pages/Graficos.jsx
+++ src/pages/Graficos.jsx
@@ -1,6 +1,7 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
 import { Pie } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
+import './Graficos.css'
 
 
 const Grafico = () => {
@@ -9,7 +10,7 @@ const Grafico = () => {
         labels: ['6-8', '8-10', '10-12', '12-14', '16-18', '18-20'], 
         datasets: [{
             label: 'Pie',
-            data: [13, 19, 27, 19, 16, 6],
+            data: [15, 22, 20, 20, 20, 3],
             backgroundColor: ['black', 'red', 'yellow', 'blue', 'orange', 'brown'],
             border: ['black', 'red', 'yellow', 'blue', 'orange', 'brown'],
         }]
@@ -21,12 +22,16 @@ const Grafico = () => {
 
     return(
         
-        <div className="GraficoTiempoDescomosturas">
-            <h1 style={{padding: '20%'}}>Grafico de Tiempo entre descomposturas</h1>
-            <div style={{width: '40%', height: '40%'}}>
+        <div className="GraficoTiempoDescomposturas">
+            <h1 style={{padding: '20%'}} className="GraficoTiempoTitulo">% de rangos cantidad de horas</h1>
+            <div style={{width: '40%', height: '40%'}} className="GraficoRangoHoras">
                 <Pie data = {data} options = {options}>
 
                 </Pie>
+            </div>
+
+            <div className="GraficoRangoReparacion">
+
             </div>
         </div>
     );
