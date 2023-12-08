c51ab0f2f7fe4400eff778647ba7b72c6aa3d9dd
AdrianL824 <adrian.lopez.or2408@gmail.com>
2023-12-07 23:44:44 -0400

    Fix: Style Historial

--- src/components/Historial.jsx
+++ src/components/Historial.jsx
@@ -10,40 +10,56 @@ const Historial = ({ lista }) => {
   };
   return (
     <Container>
-      <Table striped bordered hover>
-        <thead>
-          <tr className="columna">
-            <th>n°</th>
-            <th>Cant. Maquinas</th>
-            <th>Promedio de horas de uso</th>
-            <th>
-              Promedio de minutos <br />
-              de reparacion
-            </th>
-            <th>
-              Precio de maquina <br />
-              /hora de ocio
-            </th>
-            <th>Salario /hora</th>
-            <th>Precio ocio total</th>
-            <th>Salario total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {lista.map((i, index) => (
-            <tr key={index} className="columna">
-              <td>{index + 1}</td>
-              <td>100</td>
-              <td>{i.totalReparacion}</td>
-              <td>{i.precioHoraOcio}</td>
-              <td>{i.precioHoraOcio}</td>
-              <td>{i.precioEspecialista}</td>
-              <td>{i.precioOcioTotal}</td>
-              <td>{i.sueldoEspecialista}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <Card>
+        <Card.Header>Tabla de Resultados</Card.Header>
+        <Card.Body>
+          <Card.Text>Los resultados totales de la simulacion son:</Card.Text>
+          <Button
+            onClick={() => {
+              reload();
+            }}
+            variant="primary"
+          >
+            Actualizar
+          </Button>
+          <Container>
+            <Table striped bordered hover>
+              <thead>
+                <tr className="columna">
+                  <th>n°</th>
+                  <th>Cant. Maquinas</th>
+                  <th>Promedio de horas de uso</th>
+                  <th>
+                    Promedio de minutos <br />
+                    de reparacion
+                  </th>
+                  <th>
+                    Precio de maquina <br />
+                    /hora de ocio
+                  </th>
+                  <th>Salario /hora</th>
+                  <th>Precio ocio total</th>
+                  <th>Salario total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {lista.map((i, index) => (
+                  <tr key={index} className="columna">
+                    <td>{index + 1}</td>
+                    <td>100</td>
+                    <td>{i.totalReparacion}</td>
+                    <td>{i.precioHoraOcio}</td>
+                    <td>{i.precioHoraOcio}</td>
+                    <td>{i.precioEspecialista}</td>
+                    <td>{i.precioOcioTotal}</td>
+                    <td>{i.sueldoEspecialista}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Container>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
